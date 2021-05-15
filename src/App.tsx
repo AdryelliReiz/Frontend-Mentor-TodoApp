a9da4acef40455bd35f5d343ffa9c9ca7012ead2
Adryelli Reiz <adryellireis0santos@gmail.com>
2021-05-14 21:46:57 -0300

    Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import {GlobalStyles} from './styles/global';
+import {GlobalStyles, Main} from './styles/global';
 
 const App = () => {
   return(
     <>
-    <GlobalStyles/>
+      <GlobalStyles/>
+      <header>
+        <img src='./assets/bg-desktop-dark.jpg' alt="Banner" />
+      </header>
+      <Main>
+        <div>
+          <h1>TODO</h1>
+          <span>
+            <img src='./assets/icon-sun.svg' alt="Sun" />
+          </span>
+        </div>
+      </Main>
     </>
     
   )
